ccf6a9dfd3b1fb7faf992f8867483d7d998fea43
Wuttha <Wuttha@webmail.npru.ac.th>
2025-06-30 17:21:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World nabnoey");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening to http://localhost:" + PORT);
+});
